e66df5504a0b92a3ed12dd3721f85aa314c6b999
Rasmus Rotbart <doil@gmx.de>
2018-08-27 19:12:42 +0200

    velocity depends on distance

--- roverOne/roverOne.ino
+++ roverOne/roverOne.ino
@@ -12,20 +12,30 @@
 #define DATA_PIN 10
 
 //SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
-int maxDist = 500;
+int maxDist = 300;
 NewPing sonar(TRIG_PIN, ECHO_PIN, maxDist);
 int dist;
+int* dists;
+float momentum = 0.75;
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
+bool onoff = false;
 int angle = 45;
 int stepsize = 15;
 int numBins = 2*angle/stepsize+1;
 int count = (numBins-1)/2;
-int* dists;
+int perSide = angle/stepsize+1;
+
 byte leds;
-bool onoff = false;
+
+int veloLeft = 0;
+int veloRight = 0;
+int minVeloDist = 20;
+int maxVeloDist = 50;
+int maxBackVelo = -10;
+
 
 void updateLEDS(){
   digitalWrite(LATCH_PIN, LOW);
@@ -36,12 +46,69 @@ void updateLEDS(){
 void updateDists(){
   myservo.write(90+angle-count*stepsize);
   dist = sonar.convert_cm(sonar.ping_median(3));
-  if(dist==0) dist=666;
-  dists[count] = dist;
+  //if(dist==0) dist=666;
+  if(dist!=0){
+    dists[count] = int((1-momentum)*dists[count] + momentum*dist + 0.5);
+  } else {
+    // ??? change dist ?
+  }
   if(dist>50) bitSet(leds, count);
   else bitClear(leds, count);
-  updateLEDS();
 } // end updateDists
+
+void computeVelocity(){
+  int d = 0;
+  int dSum = 0;
+  for(int l=0; l<perSide; l++){
+    //d = (dists[l]<minVeloDist)?minVeloDist:dists[l];
+    //d = (d>maxVeloDist)?maxVeloDist:d;
+    d = (dists[l]>maxVeloDist)?maxVeloDist:dists[l];
+
+    dSum += d;
+  }
+  veloRight = 100.*float(dSum-perSide*minVeloDist)/float((maxVeloDist-minVeloDist)*perSide);
+  veloRight = (veloRight>=maxBackVelo)?veloRight:maxBackVelo;
+
+  d = 0;
+  dSum = 0;
+  for(int l=perSide; l<perSide*2; l++){
+    //d = (dists[l]<minVeloDist)?minVeloDist:dists[l];
+    //d = (d>maxVeloDist)?maxVeloDist:d;
+    d = (dists[l]>maxVeloDist)?maxVeloDist:dists[l];
+
+    dSum += d;
+  }
+  veloLeft = 100.*float(dSum-perSide*minVeloDist)/float((maxVeloDist-minVeloDist)*perSide);
+  veloLeft = (veloLeft>=maxBackVelo)?veloLeft:maxBackVelo;
+}
+
+void setupTest(){
+  updateLEDS();
+  Serial.begin(9600);
+  for(int i=0; i<numBins;i++){
+    Serial.print(0-angle+i*stepsize);
+    Serial.print("\t");
+  }
+  Serial.print(" | ");
+  Serial.print("vL");
+  Serial.print("\t");
+  Serial.print("vR");
+  Serial.println();
+  for(int i=0; i<numBins;i++){
+    Serial.print("--------");
+  }
+  Serial.print("-|------");
+  Serial.println("--------");
+  for(int i=0; i<numBins;i++){
+    Serial.print(dists[i]);
+    Serial.print("\t");
+  }
+  Serial.print(" | ");
+  Serial.print(0);
+  Serial.print("\t");
+  Serial.print(0);
+  Serial.println();
+}
 
 void setup() {
   dists = new int[numBins];
@@ -55,26 +122,9 @@ void setup() {
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
-
-  updateLEDS();
-  
-  
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.begin(9600);
-  for(int i=0; i<numBins;i++){
-    Serial.print(0-angle+i*stepsize);
-    Serial.print("\t");
-  }
-  Serial.println();
-  for(int i=0; i<numBins;i++){
-    Serial.print("--------");
-  }
-  Serial.println();
-  for(int i=0; i<numBins;i++){
-    Serial.print(dists[i]);
-    Serial.print("\t");
-  }
-  Serial.println();
+
+  setupTest();
 }
 
 void loop() {
@@ -93,6 +143,7 @@ void loop() {
       }
       count++;
       updateDists();
+      updateLEDS();
       //delay(200);
     }
   
@@ -104,11 +155,19 @@ void loop() {
       }
       count--;
       updateDists();
+      updateLEDS();
     }
+
+    computeVelocity();
+
     for(int i=0; i<numBins;i++){
       Serial.print(dists[i]);
       Serial.print("\t");
     }
+    Serial.print(" | ");
+    Serial.print(veloLeft);
+    Serial.print("\t");
+    Serial.print(veloRight);
     Serial.println();
   }
 }
